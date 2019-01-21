905dccd6727142bdd767ccb96370ed7e93c00cc0
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-01-20 21:37:34 -0500

    Use polygon

--- pcb/pcb.go
+++ pcb/pcb.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 	"math"
 	"os"
 
@@ -9,58 +10,74 @@ import (
 )
 
 var (
-	width  = flag.Float64("width", 500, "Width of svg in mm")
-	height = flag.Float64("height", 500, "Height of svg in mm")
-	step   = flag.Float64("step", 0.1, "Angle step for greating spiral in radians")
-	n      = flag.Float64("n", 10, "Number of turns in each coil")
-	trace  = flag.Float64("trace", 0.5, "Trace width in mm")
+	step  = flag.Float64("step", 0.1, "Angle step for greating spiral in radians")
+	n     = flag.Int("n", 100, "Number of turns in each coil")
+	gap   = flag.Float64("gap", 0.1, "Gap between traces in mm")
+	trace = flag.Float64("trace", 0.1, "Trace width in mm")
 
-	canvas = ponoko2d.New(os.Stdout)
+	canvas        = ponoko2d.New(os.Stdout)
+	width, height float64
+	start, end    float64
 )
 
-func background(v int) { canvas.Rect(0, 0, *width, *height, canvas.RGB(v, v, v)) }
+func background(v int) { canvas.Rect(0, 0, width, height, canvas.RGB(v, v, v)) }
 
 func main() {
-	canvas.Start(*width, *height)
+	flag.Parse()
+
+	start = 2 * math.Pi
+	end = start + float64(*n)*2*math.Pi
+	x, _ := genPt(end, *trace, 0)
+	x = 2 * math.Abs(x)
+	xl, _ := genPt(end, *trace, math.Pi)
+	xl = 2 * math.Abs(xl)
+	if xl > x {
+		x = xl
+	}
+	width = x
+	height = width
+	log.Printf("n=%v: (%.2f,%.2f)", *n, width, height)
+
+	canvas.Start(width, height)
 	background(255)
 
 	genSpiral(0)
 	genSpiral(math.Pi)
 
-	canvas.Grid(0, 0, *width, *height, 10, "stroke:black;opacity:0.1")
+	canvas.Grid(0, 0, width, height, 10, "stroke:black;opacity:0.1")
 	canvas.End()
 }
 
-func genPt(angle, tw, offset float64) (float64, float64) {
-	x := *width/2 + (angle+tw)*math.Cos(angle+offset)
-	y := *height/2 - (angle+tw)*math.Sin(angle+offset)
+func genPt(angle, halfTW, offset float64) (float64, float64) {
+	r := (angle + *trace + *gap) / (4 * math.Pi)
+	x := width/2 + (r+halfTW)*math.Cos(angle+offset)
+	y := height/2 - (r+halfTW)*math.Sin(angle+offset)
 	return x, y
 }
 
 func genSpiral(offset float64) {
-	start := 2 * math.Pi
-	end := start + float64(*n)*2*math.Pi
+	halfTW := 0.5 * *trace
 	var xs, ys []float64
 	steps := int64(math.Ceil((end - start) / *step))
 	for i := int64(0); i < steps; i++ {
 		angle := start + *step*float64(i)
-		x, y := genPt(angle, *trace, offset)
+		x, y := genPt(angle, halfTW, offset)
 		xs = append(xs, x)
 		ys = append(ys, y)
 	}
-	x, y := genPt(end, *trace, offset)
+	x, y := genPt(end, halfTW, offset)
 	xs = append(xs, x)
 	ys = append(ys, y)
-	x, y = genPt(end, -*trace, offset)
+	x, y = genPt(end, -halfTW, offset)
 	xs = append(xs, x)
 	ys = append(ys, y)
 	for i := steps - 1; i >= 0; i-- {
 		angle := start + *step*float64(i)
-		x, y := genPt(angle, -*trace, offset)
+		x, y := genPt(angle, -halfTW, offset)
 		xs = append(xs, x)
 		ys = append(ys, y)
 	}
 	xs = append(xs, xs[0])
 	ys = append(ys, ys[0])
-	canvas.Polyline(xs, ys, "stroke:black; fill:none; stroke-width:0.1")
+	canvas.Polygon(xs, ys, "stroke:none; fill:black")
 }
